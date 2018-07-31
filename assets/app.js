97a6e1a6ca12af6b94229cf40b68837f8e6076ec
Muddy Lee <muddy@Muddys-iMac.local>
2018-07-30 23:33:45 -0700

    got the still to work - 98% done

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 var preSelected = ["Trending", "Random"];
 var history = [];
+var selectedGiphs = [];
 
 $(document).ready(function(){
 
@@ -13,19 +14,19 @@ for(var i = 0; i < preSelected.length; i++){
 //load the trending and random giphs in their own divs.
 //trending
 $.ajax({
-    url: "http://api.giphy.com/v1/gifs/trending?api_key=C9oe21FaZr5JHjfQfF0o175Kjscx8dA2&limit=10&rating=PG-13",
+    url: "http://api.giphy.com/v1/gifs/trending?api_key=C9oe21FaZr5JHjfQfF0o175Kjscx8dA2&limit=12&rating=PG-13",
     method: "GET"
 }).then(function(response){
-    // console.log(response);
+    console.log(response);
     // console.log(response.data[0]);
     for (var i = 0; i < response.data.length; i++){
-    $(".trendingGiphyContainer").append(`<div id=${response}.data[i].title class="imgbox" ><a href = ${response.data[i].url}><iframe src="${response.data[i].embed_url}" width="240" height="180" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></a></div>`)
+    $(".trendingGiphyContainer").append(`<img src="${response.data[i].images.original.url}" width="240" height="180" margin= "2px" border="2px" class="giphy-embed" data-still="${response.data[i].images.original_still.url}" data-animate="${response.data[i].images.original.url}" data-state="animate"></img>`)
 
     }
 })
 
-//random
-for (var i = 0; i < 10; i++){
+//make random giphy window
+for (var i = 0; i < 12; i++){
 $.ajax({
     url: "https://api.giphy.com/v1/gifs/random?api_key=C9oe21FaZr5JHjfQfF0o175Kjscx8dA2&tag=&rating=PG-13",
     method: "GET"
@@ -36,44 +37,83 @@ $.ajax({
     // console.log(response.data.embed_url);
 
     
-    $(".randomGiphyContainer").append(`<div class="imgbox" ><a href = ${response.data.url}><iframe src="${response.data.embed_url}" width="240" height="180" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></a></div>`)
-
+    $(".randomGiphyContainer").append(`<img src="${response.data.images.original.url}" width="240" height="180" margin= "2px" border="2px" class="giphy-embed" data-still="${response.data.images.original_still.url}" data-animate="${response.data.images.original.url}" data-state="animate"></img>`)
     
 })
 }
 
+//how to make a search query
 $(document).on("click", "#add-Giphy", function(){    
     var submission = $("#giphy-input").val();
-    console.log(submission);
+    var dne = true;
+    var versionofExistence = 0;
+    for(var i = 0; i < selectedGiphs.length; i++){
+        if(submission === selectedGiphs[i]){
+            dne = false;
+            versionofExistence++;
+        }
+    }
+    //makes sure you haven't searched and made a button for it yet.
+    if (dne === true){
+        selectedGiphs.push(submission);
+        makeGiphyDiv(submission);
+    //doesn't let you make duplicate buttons, but still puts that query on top.
+    } else{
+        $.ajax({
+            url: `http://api.giphy.com/v1/gifs/search?api_key=C9oe21FaZr5JHjfQfF0o175Kjscx8dA2&q=${submission}&limit=10&offset=0&rating=PG-13&lang=en`,
+           method: "GET"
+        
+        }).then(function(response){
+            $(".giphyContainer").prepend(`<div class="${submission}${versionofExistence.toString()} container"><header><h2>${submission}</h2></header></div>`);
+            for (var i = 0; i < response.data.length; i++){
+                $(`.${submission}${versionofExistence.toString()}`).append(`<img src="${response.data[i].images.original.url}" width="240" height="180" margin= "2px" border="2px" class="giphy-embed" data-still="${response.data[i].images.original_still.url}" data-animate="${response.data[i].images.original.url}" data-state:"animate"></img>`)
+            }
+    
+        })
+        
+    }
+})
+
+// switch to still image
+ $(document).on("click", "img", function(){
+     console.log(this);
+     var dataState = $(this).attr("data-state");
+     console.log(dataState);
+     var dataAnimate = $(this).attr("data-animate");
+     var dataStill = $(this).attr("data-still");
+     
+     if(dataState === "animate"){
+        $(this).attr("src", dataStill);
+        $(this).attr("data-state", "still")
+     }
+     else{
+         this.attr("src", dataAnimate);
+         this.attr("data-state", "animate");
+     }
+    
+ })
+    
+    
+    
+//makeGiphyDiv Function
+function makeGiphyDiv(argument){
     $.ajax({
-        url: `http://api.giphy.com/v1/gifs/search?api_key=C9oe21FaZr5JHjfQfF0o175Kjscx8dA2&q=${submission}&limit=10&offset=0&rating=PG-13&lang=en`,
+        url: `http://api.giphy.com/v1/gifs/search?api_key=C9oe21FaZr5JHjfQfF0o175Kjscx8dA2&q=${argument}&limit=10&offset=0&rating=PG-13&lang=en`,
        method: "GET"
     
     }).then(function(response){
-       console.log(response); 
-        $(".selectedButtonsContainer").prepend(`<button id=${submission}>${submission}</button>`);
-        $(".giphyContainer").prepend(`<div class="${submission}Container container"><header><h2>${submission}</h2></header></div>`);
+ 
+        $(".selectedButtonsContainer").prepend(`<button id=${argument}>${argument}</button>`);
+        $(".giphyContainer").prepend(`<div class="${argument}Container container"><header><h2>${argument}</h2></header></div>`);
         for (var i = 0; i < response.data.length; i++){
-            $(`.${submission}Container`).append(`<div class="imgbox" ><a href = ${response.data[i].url}><iframe src="${response.data[i].embed_url}" width="240" height="180" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></a></div>`)
+            $(`.${argument}Container`).append(`<img src="${response.data[i].images.original.url}" width="240" height="180" margin= "2px" border="2px" class="giphy-embed" data-still="${response.data[i].images.original_still.url}" data-animate="${response.data[i].images.original.url}" data-state="animate"></img>`)
         }
 
     })
-    
-
-})
-
-// $(document).on("click", "button", function(){
-//     var topic = this.text();
-//     $.ajax({
-//         url: 
-//     })
 
 
-
-
-
-// })
-
+}
 
 
 })
+
