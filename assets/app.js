ba85e5c9932b27e53c69113a9e05d8b383467db3
Ken Brummage <KBrummage@gmail.com>
2019-04-07 19:24:57 -0700

    set up trial for download button on trending

--- assets/app.js
+++ assets/app.js
@@ -3,8 +3,6 @@ var history = [];
 var selectedGiphs = [];
 
 $(document).ready(function(){
-
-
 //make preSelected buttons
     for(var i = 0; i < preSelected.length; i++){
         $(".selectedButtonsContainer")
@@ -17,19 +15,31 @@ $(document).ready(function(){
         url: "https://api.giphy.com/v1/gifs/trending?api_key=C9oe21FaZr5JHjfQfF0o175Kjscx8dA2&tag=&rating=PG-13",
         method: "GET"
     }).then(function(response){
-        console.log(response)
+
         for (var i = 0; i < 10; i++){
             var tempDiv = $("<div>")
             tempDiv.attr("id", `${i}TrendingDiv`)
-            
+            var tempBtnDiv = $("<div>")
+            tempBtnDiv.attr("class", "btnDiv")
+            var tempA = $("<a>")
+            tempA.attr("target", "_blank")
+                .attr("href", `${response.data[i].images.original.url}`)
+                .attr("style", "text-decoration: none")
+
             var tempImg = $("<img>")
+            
             tempImg.attr("src", response.data[i].images.original.url)
             .attr("data-still", response.data[i].images.original_still.url)
             .attr("data-animate", response.data[i].images.original.url)
             .attr("data-state", "animate")
             .attr("class", "giphy-embed")
+            var downBtn = $("<div class='btnBtn'>Download</div>")
             
+            tempA.append(downBtn)
+            tempBtnDiv.append(tempA)
             tempDiv.append(tempImg)
+            tempDiv.append(tempBtnDiv)
+            tempDiv.attr("style", "background-color: white; margin: 2px;")
         $(".trendingGiphyContainer").append(tempDiv)
 
         }
@@ -43,11 +53,19 @@ $(document).ready(function(){
         url: "https://api.giphy.com/v1/gifs/random?api_key=C9oe21FaZr5JHjfQfF0o175Kjscx8dA2&tag=&rating=PG-13",
         method: "GET"
     }).then(function(response){    
-        $(".randomGiphyContainer").append(`<div id="${i}div"><img src="${response.data.images.original.url}" id="${i}" width="240" height="180" margin= "2px" border="2px" class="giphy-embed" data-still="${response.data.images.original_still.url}" data-animate="${response.data.images.original.url}" data-state="animate"></img>`)
+        $(".randomGiphyContainer").append(`<div id="${i}div">
+                                                <img src="${response.data.images.original.url}" 
+                                                        id="${i}" width="240" height="180" 
+                                                        margin= "2px" border="2px" 
+                                                        class="giphy-embed" 
+                                                        data-still="${response.data.images.original_still.url}" 
+                                                        data-animate="${response.data.images.original.url}" 
+                                                        data-state="animate">
+                                                </img>
+                                            </div>`)
         
     })
     }
-
     //how to make a search query
     $(document).on("click", "#add-Giphy", function(){  
         if($("#giphy-input").val().trim() !== ""){  
@@ -75,7 +93,7 @@ $(document).ready(function(){
         //doesn't let you make duplicate buttons, but still puts that query on top.
         } else{
             $.ajax({
-                url: `https://api.giphy.com/v1/gifs/search?api_key=C9oe21FaZr5JHjfQfF0o175Kjscx8dA2&q=${submission}&limit=10&offset=0&rating=PG-13&lang=en`,
+                url: `https://api.giphy.com/v1/gifs/search?api_key=C9oe21FaZr5JHjfQfF0o175Kjscx8dA2&q=${submission}&limit=12&offset=0&rating=PG-13&lang=en`,
             method: "GET"
             
             }).then(function(response){
@@ -89,7 +107,6 @@ $(document).ready(function(){
         }
     }
     })
-
     // switch to still image
     $(document).on("click", "img", function(){
         console.log(this);
@@ -117,20 +134,18 @@ $(document).ready(function(){
         
     })
         
-        
-        
+                
     //makeGiphyDiv Function
     function makeGiphyDiv(argument, BannerName){
         $.ajax({
             url: `http://api.giphy.com/v1/gifs/search?api_key=C9oe21FaZr5JHjfQfF0o175Kjscx8dA2&q=${argument}&limit=10&offset=0&rating=PG-13&lang=en`,
         method: "GET"
-        
         }).then(function(response){
     
             $(".selectedButtonsContainer").prepend(`<button id=${argument}>${BannerName}</button>`);
             $(".giphyContainer").prepend(`<div class="${argument}Container container"><header><h2>${BannerName}</h2></header></div>`);
             for (var i = 0; i < response.data.length; i++){
-                $(`.${argument}Container`).append(`<div id="${argument}${i}div" class="giphy-embed"><a href=${response.data[i].images.original.url} download><img src="${response.data[i].images.original.url}" width="240" height="180" margin= "2px" border="2px" class="giphy-embed" data-still="${response.data[i].images.original_still.url}" id="${argument}${i}" data-animate="${response.data[i].images.original.url}" data-state="animate"></img></a></div>`)
+                $(`.${argument}Container`).append(`<div id="${argument}${i}div" class="giphy-embed"><img src="${response.data[i].images.original.url}" width="240" height="180" margin= "2px" border="2px" class="giphy-embed" data-still="${response.data[i].images.original_still.url}" id="${argument}${i}" data-animate="${response.data[i].images.original.url}" data-state="animate"></img></div>`)
             }
 
         })
